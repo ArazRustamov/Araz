d96cf77a18f3674885d4becb67aaa66ffd967a00
Araz Rustamov <34452927+ArazRustamov@users.noreply.github.com>
2017-12-15 16:03:51 +0400

    Even and Odds

---
+++ script.js
@@ -0,0 +1,7 @@
+var a= parseInt(Number(prompt("Zehmet olmasa a reqemini daxil edin.")));
+var b= parseInt(Number(prompt("Zehmet olmasa b reqemini daxil edin.")));
+var c= parseInt(Number(prompt("Zehmet olmasa c reqemini daxil edin.")));
+
+if(a%2 == 0 && b%2==0 && c%2==0||a%2 != 0 && b%2!=0 && c%2!=0) {
+console.log("NO")
+} else {console.log("Yes")}
