3c3083cb4f198fa3c1db3e167252fc40b0cc6caa
Araz Rustamov <34452927+ArazRustamov@users.noreply.github.com>
2017-12-15 16:24:30 +0400

    Warderob

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var a= parseInt(Number(prompt("Zehmet olmasa a reqemini daxil edin.")));
+/*var a= parseInt(Number(prompt("Zehmet olmasa a reqemini daxil edin.")));
 var b= parseInt(Number(prompt("Zehmet olmasa b reqemini daxil edin.")));
 var c= parseInt(Number(prompt("Zehmet olmasa c reqemini daxil edin.")));
 
 if(a%2 == 0 && b%2==0 && c%2==0||a%2 != 0 && b%2!=0 && c%2!=0) {
-console.log("NO")
-} else {console.log("Yes")}
+console.log("NO"}
+} else {console.log("Yes")} */
+
+
+/************************************************************************************** */
+
+
+
+var a= parseInt(Number(prompt("Salam zehmet olmasa Qarderobun Hundurluyunu daxil edin.")));
+var b= parseInt(Number(prompt("Salam zehmet olmasa Qarderobun enini  daxil edin.")));
+var c= parseInt(Number(prompt("Salam zehmet olmasa Qarderobun qalinligini  daxil edin.")));
+
+var x= parseInt(Number(prompt("Qapinin hundurluyu")));
+var y= parseInt(Number(prompt("Qapinin eni")));
+
+if(a<x && c<y ) { console.log("YES")
+
+}else {console.log("NO")}
